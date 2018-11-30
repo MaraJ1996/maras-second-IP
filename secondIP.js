766cbe796f99a90b53f3ba604bad83f3b4858c5b
Mara Jackson <mjadejackson@gmail.com>
2018-11-30 12:35:55 +0300

    defines sides A,B,C as variables. ParseInt, prompts each side

--- secondIP.js
+++ secondIP.js
@@ -1,1 +1,3 @@
-<link href="secondIP.css" rel="stylesheet" type="text/css">
+var sideA = parseInt(prompt("By typing in three numbers, you can use this tracker to determine the type of triangle your numbers form. What is the value for sideA?"));
+var sideB = parseInt(prompt("What is the value for sideB?"));
+var sideC = parseInt(prompt("What is the value for sideC?"));
