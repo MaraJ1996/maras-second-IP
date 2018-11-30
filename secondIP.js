42a652dea4a8b857ef0ba9e6f107cd11d0e3a9e6
Mara Jackson <mjadejackson@gmail.com>
2018-11-30 12:49:59 +0300

    removed incorrect semicolon

--- secondIP.js
+++ secondIP.js
@@ -11,4 +11,4 @@ else if (sideA === sideB || sideB === sideC || sideC === sideA) {
 else if ( sideA !== sideB && sideB !== sideC && sideC !== sideA) {
     alert("Scalene");
     }
-else ( sideA + sideB !== sideC || sideB + sideC !== sideA || sideA + sideC !== sideB);
+else ( sideA + sideB !== sideC || sideB + sideC !== sideA || sideA + sideC !== sideB)
