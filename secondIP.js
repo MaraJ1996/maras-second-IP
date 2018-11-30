47be84d547a319901765d604c8a961c940e9b6fd
Mara Jackson <mjadejackson@gmail.com>
2018-11-30 16:41:44 +0300

    set parameters for each triangle type in js

--- secondIP.js
+++ secondIP.js
@@ -3,3 +3,18 @@ function checkinputs() {
 var sideA = parseInt(document.getElementById("a").value);
 var sideB = parseInt(document.getElementById("b").value);
 var sideC = parseInt(document.getElementById("c").value);
+
+if (sideA === sideB && sideB === sideC) {
+    document.getElementById("result").value="Equilateral"
+    }
+else if (sideA === sideB || sideB === sideC || sideC === sideA) {
+    document.getElementById("result").value="Isosceles"
+    }
+else if ( sideA != sideB != sideC) {
+     if ((( sideA + sideB) <= sideC) || (( sideB + sideC) <= sideA) || (( sideC + sideA) <= sideB))) {
+  document.getElementById("result").value="Not a triangle"
+    }
+    else  {
+        document.getElementById("result").value="Scalene"
+        }
+}
