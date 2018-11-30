82a6ac5aec7d6cdd02db632490a715f995d8917d
Mara Jackson <mjadejackson@gmail.com>
2018-11-30 13:18:18 +0300

    adds side definitions and parameters to html

--- secondIP.js
+++ secondIP.js
@@ -1,3 +1,4 @@
+{
 var sideA = parseInt(prompt("By typing in three numbers, you can use this tracker to determine the type of triangle your numbers form. What is the value for sideA?"));
 var sideB = parseInt(prompt("What is the value for sideB?"));
 var sideC = parseInt(prompt("What is the value for sideC?"));
@@ -14,3 +15,4 @@ else if ( sideA !== sideB && sideB !== sideC && sideC !== sideA) {
 else ( sideA + sideB !== sideC || sideB + sideC !== sideA || sideA + sideC !== sideB) {
     alert("Not a triangle");
     }
+}
