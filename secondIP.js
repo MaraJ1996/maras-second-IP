7700ae7fb43398091755b5b12096f3c5459aca4d
Mara Jackson <mjadejackson@gmail.com>
2018-11-30 12:39:02 +0300

    sets four spaces for if else statements

--- secondIP.js
+++ secondIP.js
@@ -1,3 +1,8 @@
 var sideA = parseInt(prompt("By typing in three numbers, you can use this tracker to determine the type of triangle your numbers form. What is the value for sideA?"));
 var sideB = parseInt(prompt("What is the value for sideB?"));
 var sideC = parseInt(prompt("What is the value for sideC?"));
+
+if
+else if
+else if
+else
